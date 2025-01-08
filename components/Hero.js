c2e8812e4cc17407191ef5899dfa35a521b5517d
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2025-01-09 03:14:08 +0530

    Thursday push

--- components/Hero.js
+++ components/Hero.js
@@ -1,26 +1,27 @@
-import React from "react";
+import HeroCard from "./HeroCard";
 
 const Hero = () => {
   return (
     <section className="h-screen relative">
       <h1 className="text-center shadow-[0px_4px_4px_0px_#00000040] bg-gradient-to-b from-[#F24181] to-[#000000] text-transparent bg-clip-text leading-none">
-        <span className="md:text-[250px] marck-script-regular">E-</span>
+        <span className="md:text-[250px] marck-script-regular">E</span>
         <span className="squada-one-regular md:text-[200px]">
-          SUMMIT&apos;25
+          -SUMMIT&apos;25
         </span>
       </h1>
-      <div className="absolute bottom-1/3 right-1/3">
-        <h2 className="roboto-medium tracking-[-8%] md:text-[100px] uppercase md:leading-[90px]">
+      <div className="absolute bottom-[40%] left-[10%] md:left-[15%]">
+        <h2 className="roboto-medium tracking-[-8%] md:text-[80px] uppercase md:leading-[80px]">
           We Build <br />
           Entrepreneurs
         </h2>
-        <p className="roboto-light md:text-[38px] md:leading-[31.16px]">
+        <p className="roboto-light md:text-[32px] md:leading-[30px]">
           Quality you won't find elsewhere.
         </p>
-        <button className="border-[3px] border-white rounded-[180px] roboto-semibold text-[24px] leading-[28.13px] px-8 py-4 mt-8 ml-8 hover:bg-white hover:text-black">
+        <button className="border-[3px] border-white rounded-[180px] roboto-semibold text-[20px] leading-[24px] px-8 py-4 mt-8 ml-8 hover:bg-white hover:text-black">
           Get Started
         </button>
       </div>
+      <HeroCard />
     </section>
   );
 };
