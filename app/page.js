b0a97fbcdb73439cb1d1881444e99bf7a9bf4726
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2025-02-06 18:31:02 +0530

    Kaustubh's commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import About from "../components/About";
 import Sponsors from "../components/Sponsors";
 import HeroComponent from "../components/HeroComponent";
-import WhyECellDTU from "../components/whyecell";
-import EventGrid from "../components/events";
+import WhyECellDTU from "../components/Whyecell";
+import EventGrid from "../components/Events";
 import { AnimatedPinDemo } from "../components/Animatedpin";
 import CarouselDemo from "../components/CarouselDemo";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
