45bbb17ee8e592f0c2bbe76ba1b29800c2c5c521
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2025-03-07 17:55:36 +0530

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,12 +1,15 @@
-import { doc, setDoc } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "@/firebaseConfig";
 import { NextResponse as res } from "next/server";
 
 export async function POST(req) {
   try {
     const data = await req.json();
-    await setDoc(doc(db, "registrations", data.email), { ...data });
-    return res.json({ message: "Registration Successful" }, { status: 200 });
+    // Reference to the 'registrations' collection
+    const registrationsRef = collection(db, "registrations");
+    // Add a new document with an auto-generated ID
+    const docRef = await addDoc(registrationsRef, data);
+    return res.json({ message: "Registration Successful", id: docRef.id }, { status: 200 });
   } catch (error) {
     console.error(error);
     return res.json({ error: "Registration Failed" }, { status: 400 });
